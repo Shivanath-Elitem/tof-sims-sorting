c6860bbb878ede8268172ec5441ffdbfc2b453cf
jettrainz <ryandisanti5810@gmail.com>
2023-09-11 13:26:06 -0500

    wip

--- Database/DatabaseConnector.py
+++ Database/DatabaseConnector.py
@@ -7,7 +7,34 @@ async def database_init():
     print("[SQLite] Starting...")
     global db
     db = await aiosqlite.connect("sqlite.db")
+    
     await provision_database(db)
     
     print("[SQLite] Started!")
+
+
+class AsyncDatabase:
     
+    def __init__(self, file):
+        self.file = file
+    
+    
+    '''execute()
+    exec_cmd: str = SQL command to execute
+    p: bool = Print SQL command before executing
+    '''
+    async def execute(self, exec_cmd: str, p=False):
+        if p: print(exec_cmd)
+        
+        if exec_cmd.startswith("SELECT"):
+            val = await db.execute_fetchall(exec_cmd)
+            if len(val) == 1:
+                if len(val[0]) == 1:
+                    return val[0][0]
+            return val
+        else:
+            await db.execute(exec_cmd)
+            await db.commit()
+    
+    def exists(self, rows):
+        return rows > 0
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import asyncio
-from Database.DatabaseConnector import database_init
+from Database.DatabaseConnector import database_init, AsyncDatabase
+
+db = AsyncDatabase("main.py")
 
 print("ToF SIMS Sorting Program")
 
 async def main():
     await database_init()
+    await db.execute(
+        "INSERT INTO TEST values (`0`)"
+    )
+    val = await db.execute("SELECT * FROM TEST")
+    print(val)
+    
+    
 
 if __name__ == "__main__": asyncio.run(main())
